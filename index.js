3bb4b20dad23a3e429ccaae3821086784bfa2bbf
Faith <faithbrianna01@gmail.com>
2023-12-19 07:36:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+
+const names = ['Guadalupe', 'Ollie', 'Aki']
+
+ function writeCards(names, event) {
+    let message = [];
+    for (let i = 0; i < names.length; i++) {
+        message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return message;
+ }
+ const event = 'surprise'
+ const message = writeCards(names, event)
+
+
+ function countDown(numbers) {
+    while (numbers >= 0) {
+        console.log(numbers);
+        numbers--;
+    }
+ }
+ countDown(10)
